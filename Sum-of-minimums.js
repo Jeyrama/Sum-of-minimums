18249f3c6b03c08de0bc6bd23d2cad21dd7bc3b0
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-22 19:56:24 -0700

    Update Sum-of-minimums.js

--- Sum-of-minimums.js
+++ Sum-of-minimums.js
@@ -7,9 +7,12 @@ For Example:
   , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
   , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
   ]
-  
+
   So the function should return 26 because 
   the sum of the minimums is 1 + 5 + 20 = 26.
 
 Note: You will always be given a non-empty list containing positive values.
 */
+
+
+// Solution
