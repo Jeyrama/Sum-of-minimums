467f4e574cb6e029ffec8d2ba27446b3a8330757
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-22 19:56:54 -0700

    Update Sum-of-minimums.js

--- Sum-of-minimums.js
+++ Sum-of-minimums.js
@@ -22,3 +22,13 @@ function sumOfMinimums(arr) {
 }
 
 // or
+
+function sumOfMinimums(arr) {
+  let total = 0;
+  
+  for (let i = 0; i < arr.length; i++){
+   total += Math.min(...arr[i])
+  }
+  
+  return total;
+}
